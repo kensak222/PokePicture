48397f2d4970e18f0f08eb952e7a30d3a3fe7a47
kensak222 <66297692+kensak222@users.noreply.github.com>
2022-01-26 20:24:31 +0900

    update: PokeListFragmentのセットアップが完了

--- app/src/main/java/com/example/pokepicture/PokeListFragment.kt
+++ app/src/main/java/com/example/pokepicture/PokeListFragment.kt
@@ -1,59 +1,58 @@
 package com.example.pokepicture
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.pokepicture.databinding.FragmentPokeListBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PokeListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PokeListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+//    private val pokeAdapter: PokeAdapter by lazy {
+//        PokeAdapter()
+//    }
+
+    private var _binding: FragmentPokeListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_poke_list, container, false)
+        Log.d(TAG, this.resources.getString(R.string.on_create_view_called))
+
+        _binding = FragmentPokeListBinding.inflate(inflater, container, false)
+
+        // recycleViewのセットアップ
+        val decoration = DividerItemDecoration(requireContext(), LinearLayoutManager(requireContext()).orientation)
+//        binding.pokeRecyclerView.apply {
+//            addItemDecoration(decoration)
+//            adapter = pokeAdapter
+//            layoutManager = LinearLayoutManager(requireContext())
+//            setHasFixedSize(true)
+//        }
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, this.resources.getString(R.string.on_destroy_called))
+
+        super.onDestroy()
+        _binding = null
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PokeListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PokeListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        private val TAG = PokeListFragment::class.java.simpleName
+
+        // TODO: 必要なければ削除
+//        fun newInstance() = FavoriteFragment()
+
+        // FIXME: ダミーデータなので、削除する
+        private val dummyData = List<String>(100) {i -> "dummy$i"}
     }
 }
