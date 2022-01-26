1969c14ab14e954c7d4e5459c4fc2cafcc0611fa
kensak222 <66297692+kensak222@users.noreply.github.com>
2022-01-26 20:06:12 +0900

    add: values配下でOKな（ブルドエラーにならない）ファイルを追加

--- app/src/main/java/com/example/pokepicture/MainActivity.kt
+++ app/src/main/java/com/example/pokepicture/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.pokepicture
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// strings or themes or colors に問題あり
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
