ffd6ac25b1ffedb5214a0f3269de48fe0c602b21
kensak222 <66297692+kensak222@users.noreply.github.com>
2022-01-26 20:12:24 +0900

    add: 改行追加

--- app/src/main/java/com/example/pokepicture/MainActivity.kt
+++ app/src/main/java/com/example/pokepicture/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.pokepicture
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-// strings or themes or colors に問題あり
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
