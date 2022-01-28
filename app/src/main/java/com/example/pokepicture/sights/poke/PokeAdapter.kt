58ec9bc3bcafbed90145b0a2def00063ce5ba285
kensak222 <66297692+kensak222@users.noreply.github.com>
2022-01-29 00:06:40 +0900

    add: FavoriteFragmentを追加

--- app/src/main/java/com/example/pokepicture/sights/poke/PokeAdapter.kt
+++ app/src/main/java/com/example/pokepicture/sights/poke/PokeAdapter.kt
@@ -1,4 +1,4 @@
-package com.example.pokepicture
+package com.example.pokepicture.sights.poke
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
--- app/src/main/java/com/example/pokepicture/sights/poke/PokeListFragment.kt
+++ app/src/main/java/com/example/pokepicture/sights/poke/PokeListFragment.kt
@@ -1,4 +1,4 @@
-package com.example.pokepicture
+package com.example.pokepicture.sights.poke
 
 import android.os.Bundle
 import android.util.Log
