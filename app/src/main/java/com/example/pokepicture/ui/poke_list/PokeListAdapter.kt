74bf35da3f551d979a9ff6b8670b3547c74e8bb8
kensak222 <66297692+kensak222@users.noreply.github.com>
2022-01-29 14:44:14 +0900

    add: Adapterのライフサイクルコールメッセージを追加

--- app/src/main/java/com/example/pokepicture/ui/poke_list/PokeListAdapter.kt
+++ app/src/main/java/com/example/pokepicture/ui/poke_list/PokeListAdapter.kt
@@ -1,13 +1,18 @@
 package com.example.pokepicture.ui.poke_list
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.pokepicture.R
 import com.example.pokepicture.databinding.PokeRowLayoutBinding
 
-class PokeListAdapter : RecyclerView.Adapter<PokeListAdapter.PokeViewHolder>() {
+class PokeListAdapter : RecyclerView.Adapter<PokeListAdapter.PokeListViewHolder>() {
 
-    class PokeViewHolder(private val binding: PokeRowLayoutBinding) :
+    private lateinit var context: Context
+
+    class PokeListViewHolder(private val binding: PokeRowLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(position: Int) {
@@ -15,25 +20,29 @@ class PokeListAdapter : RecyclerView.Adapter<PokeListAdapter.PokeViewHolder>() {
                 pictureBookNo.text = position.toString()
             }
         }
-
-        companion object {
-            fun from(parent: ViewGroup): PokeViewHolder {
-                val layoutInflater = LayoutInflater.from(parent.context)
-                val binding = PokeRowLayoutBinding.inflate(layoutInflater, parent, false)
-                return PokeViewHolder(binding)
-            }
-        }
     }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): PokeViewHolder = PokeViewHolder.from(parent)
+    ): PokeListViewHolder {
+        context = parent.context
+        Log.d(TAG, context.resources.getString(R.string.on_create_view_holder_called))
 
-    override fun onBindViewHolder(holder: PokeViewHolder, position: Int) {
+        val layoutInflater = LayoutInflater.from(context)
+        val binding = PokeRowLayoutBinding.inflate(layoutInflater, parent, false)
+         return PokeListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holderList: PokeListViewHolder, position: Int) {
+        Log.d(TAG, context.resources.getString(R.string.on_bind_view_holder_called))
         // TODO: Entityとのつなぎこみを行う
-        holder.bind(position)
+        holderList.bind(position)
     }
 
     override fun getItemCount(): Int = 500
+
+    companion object {
+        private val TAG = PokeListAdapter::class.java.simpleName
+    }
 }
