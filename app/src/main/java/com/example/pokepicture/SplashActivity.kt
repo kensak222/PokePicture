76cdd6292b59fae5c9a908f1c33cbf2f5cbebd1e
kensak222 <66297692+kensak222@users.noreply.github.com>
2022-01-31 22:06:13 +0900

    update: 軽微な調整

--- app/src/main/java/com/example/pokepicture/SplashActivity.kt
+++ app/src/main/java/com/example/pokepicture/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.example.pokepicture
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -12,12 +13,17 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Log.d(TAG, this.resources.getString(R.string.on_create_called))
 
         CoroutineScope(Dispatchers.Main).launch {
-            delay(2000)
+            delay(4000)
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
+    }
+
+    companion object{
+        private val TAG = SplashActivity::class.java.simpleName
     }
 }
